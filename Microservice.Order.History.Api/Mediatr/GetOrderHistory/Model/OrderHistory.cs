4d7f8286cd1adfc42388a83fc033ef245553be65
John Miller <john.miller@unionstreet.co.uk>
2024-08-26 12:46:59 +0100

    Fix default issues

--- Microservice.Order.History.Api/Mediatr/GetOrderHistory/Model/OrderHistory.cs
+++ Microservice.Order.History.Api/Mediatr/GetOrderHistory/Model/OrderHistory.cs
@@ -4,12 +4,12 @@ public class OrderHistory
 {
     public Guid Id { get; set; }
     public Guid CustomerId { get; set; }
-    public string AddressSurname { get; set; }
-    public string AddressForename { get; set; }
-    public string OrderNumber { get; set; }
-    public string OrderStatus { get; set; }
-    public List<OrderItemHistory> OrderItems { get; set; }
+    public string AddressSurname { get; set; } = string.Empty;
+    public string AddressForename { get; set; } = string.Empty;
+    public string OrderNumber { get; set; } = string.Empty;
+    public string OrderStatus { get; set; } = string.Empty;
+    public List<OrderItemHistory> OrderItems { get; set; } = default!;
     public DateOnly OrderPlaced { get; set; }
     public decimal Total { get; set; }
-    public OrderHistoryAddress Address { get; set; }
+    public OrderHistoryAddress Address { get; set; } = default!;
 }
